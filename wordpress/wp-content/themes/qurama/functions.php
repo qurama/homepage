ca1f2e67a1eba2e9462129980af3974fc281f63b
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-28 06:45:42 +0700

    enqueue style

--- wordpress/wp-content/themes/qurama/functions.php
+++ wordpress/wp-content/themes/qurama/functions.php
@@ -4,10 +4,29 @@
 * homepage : http://selesdepselesnul.com
 * company  : qurama studios
 */
+// <link href="css/animate.min.css" rel="stylesheet"> 
+// <link href="css/font-awesome.min.css" rel="stylesheet">
+// <link href="css/lightbox.css" rel="stylesheet">
+// <link href="css/main.css" rel="stylesheet">
+// <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
+// <link href="css/responsive.css" rel="stylesheet">
+
 
 define(CSS_PATH, get_template_directory_uri().'/css');
 function add_scripts() {
 	wp_enqueue_style('bootstrap.min', CSS_PATH . '/bootstrap.min.css');
+	wp_enqueue_style('animate.min', CSS_PATH . '/animate.min.css', 
+		array('bootstrap.min'));
+	wp_enqueue_style('font-awesome.min', CSS_PATH . '/font-awesome.min.css', 
+		array('animate.min'));
+	wp_enqueue_style('lightbox', CSS_PATH . '/lightbox.css', 
+		array('font-awesome.min'));
+	wp_enqueue_style('main', CSS_PATH . '/main.css', 
+		array('lightbox'));
+	wp_enqueue_style('css-preset', CSS_PATH . '/presets/preset1.css', 
+		array('main'));
+	wp_enqueue_style('responsive', CSS_PATH . 'responsive.css', 
+		array('css-preset'));
 }
 
 add_action('wp_enqueue_scripts', 'add_scripts');

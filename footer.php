694837d25bf7c6530865bc0c0a40c0a207a5b0a5
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-29 17:49:42 +0700

    add copyright year

--- footer.php
+++ footer.php
@@ -4,10 +4,11 @@
         <div class="footer-logo">
           <a href="#"><img class="img-responsive" 
           src="<?php echo IMAGE_PATH . '/logo.png'; ?>" alt=""></a>
+          &copy<?php echo date('Y') ?>
         </div>
       </div>
     </div>
-    
+
   </footer>
 
   <?php wp_footer(); ?>
